04cc11e04749d362d386cbf786da533a158a51c6
Kroszborg <abhimanpanwar6@gmail.com>
2025-02-18 01:06:56 +0530

    added basic site to check deployment

--- app/thank-you/loading.tsx
+++
@@ -1,7 +0,0 @@
-export default function Loading() {
-  return (
-    <div className="flex items-center justify-center w-screen h-screen">
-      <div className="text-white text-center">Loading...</div>
-    </div>
-  );
-}
