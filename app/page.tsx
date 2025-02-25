7f938c9707229eddea2ca2a7c3ebc8a116fc4667
Kroszborg <abhimanpanwar6@gmail.com>
2025-02-25 23:15:55 +0530

    added metadata and icon

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,39 @@
-"use client";
-
 import type React from "react";
 import HeroSection from "@/components/HeroSection";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  metadataBase: new URL("https://example.com"),
+  title: "Sparkonomy",
+  description: "Welcome to Sparkonomy - Let's Build The Future Together!",
+  openGraph: {
+    title: "Sparkonomy",
+    description: "Welcome to Sparkonomy - Let's Build The Future Together!",
+    images: [
+      {
+        url: "/Sparkonomy.jpg",
+        width: 1200,
+        height: 630,
+        alt: "Sparkonomy Thumbnail",
+      },
+    ],
+    locale: "en_IND",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Sparkonomy",
+    description: "Welcome to Sparkonomy - Let's Build The Future Together!",
+    images: ["/Sparkonomy.jpg"],
+  },
+};
 
 export default function Home() {
-
   return (
     <main className="overflow-hidden">
       <div className="">
         <div className="flex-grow">
-          <HeroSection/>
+          <HeroSection />
         </div>
       </div>
     </main>
