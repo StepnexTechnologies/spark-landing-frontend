769e789378c9e6c94fedd206f326424d00172795
naad <naad.229301420@muj.manipal.edu>
2025-10-24 23:49:44 +0530

    feat: implement FAQ section with dynamic category filtering and enhance CTAButton functionality

--- app/creator/earn/faqs/page.tsx
+++ app/creator/earn/faqs/page.tsx
@@ -11,7 +11,7 @@ export default function CreatorEarnFAQsPage() {
             {/* Background Gradients */}
             <div className="absolute -top-[400px] -left-[940px] inset-0 pointer-events-none">
                 {/* Pink to Purple Gradient Blob */}
-                <div className="mt-[500px] w-[2422px] h-[40%] md:w-[4500px] md:h-[2422px] gradient-blob" />
+                <div className="mt-[500px] w-[2422px] h-[34%] md:w-[4500px] gradient-blob" />
                 <div className="-mt-[1000px] w-[2422px] h-[66%] md:w-[4500px] gradient-blob" />
             </div>
 
--- components/creator/earn/CTAButton.tsx
+++ components/creator/earn/CTAButton.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Link from "next/link";
 
 interface CTAButtonProps {
     buttonText?: string;
@@ -8,9 +9,9 @@ interface CTAButtonProps {
 
 const CTAButton = ({buttonText = "Send Invoices For Free", className, navigateTo = "https://dev.creator.sparkonomy.com/auth?service=invoice"}: CTAButtonProps) => {
     return (
-        <button
+        <Link
             className={`all-[unset] box-border inline-flex items-start p-0.5 [-webkit-backdrop-filter:blur(16px)_brightness(100%)] relative flex-col rounded-[32px] backdrop-blur-[2px] backdrop-brightness-[100%] gap-2.5 bg-[#ffffff1a] border-[none] before:content-[''] before:[mask-composite:exclude] before:pointer-events-none before:inset-0 before:[background:linear-gradient(162deg,rgba(221,42,123,1)_0%,rgba(151,71,255,1)_64%)] before:absolute before:[-webkit-mask:linear-gradient(#fff_0_0)_content-box,linear-gradient(#fff_0_0)] before:rounded-[32px] before:p-px before:z-[1] before:[-webkit-mask-composite:xor] ${className}`}
-            onClick={() => window.open(navigateTo, "_blank")}
+            href={navigateTo}
         >
             <div
                 className={`all-[unset] box-border items-center gap-2.5 flex-[0_0_auto] px-4 py-2.5 rounded-[32px] justify-center relative bg-[linear-gradient(162deg,rgba(221,42,123,0.4)_0%,rgba(151,71,255,0.4)_64%)] inline-flex shadow-[inset_0_1px_0_rgba(255,255,255,0.40),inset_1px_0_0_rgba(255,255,255,0.32),inset_0_-1px_1px_rgba(0,0,0,0.13),inset_-1px_0_1px_rgba(0,0,0,0.11)] [-webkit-backdrop-filter:blur(2.0px)_brightness(100.0%)_saturate(100.0%)] backdrop-blur-[2.0px] backdrop-brightness-[100.0%] backdrop-saturate-[100.0%]`}
@@ -19,7 +20,7 @@ const CTAButton = ({buttonText = "Send Invoices For Free", className, navigateTo
                     {buttonText}
                 </div>
             </div>
-        </button>
+        </Link>
     );
 };
 
