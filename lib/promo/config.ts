4f7f03cde065048f8354c78095465a703be0012c
Chirag Bansal <chiragbansal933@gmail.com>
2026-05-01 09:56:52 +0530

    feat: Live

--- lib/promo/config.ts
+++ lib/promo/config.ts
@@ -42,7 +42,7 @@ function dateRange(
 
 // Summer Sign-up Daily Vouchers Promo: 2nd May 2026 – 31st May 2026.
 const SUMMER_SIGNUP_WINDOW = dateRange(
-  { year: 2026, month: 3, day: 1 },
+  { year: 2026, month: 4, day: 2 },
   { year: 2026, month: 4, day: 31 },
 );
 
